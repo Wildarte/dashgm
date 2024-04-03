6f1e6073d05b19a769110b407ab0383844fbd507
wildarte <wildarte10@gmail.com>
2024-04-02 23:42:03 -0300

    comecei a fazer o javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,47 @@ body_dash.style.width = "calc(100% - "+width_side_menu+"px)";
 
 
 
+// ===================== modal =================================
+const modal = document.querySelectorAll('.modal');
+modal.forEach((item, index) => {
+
+    let itens =  item.querySelectorAll('.form_modal input');
+    let lab = item.querySelectorAll('.form_modal label')
+
+    itens.forEach((item2, index) => {
+        item2.addEventListener('focus', () => {
+            lab.forEach((item3) => {
+                item3.classList.remove('onfocus')
+            })
+
+            lab[index].classList.add('onfocus');
+        })
+    })
+    
+    itens.forEach((item2, index) => {
+        item2.addEventListener('focusout', () => {
+            lab.forEach((item3) => {
+                item3.classList.remove('onfocus')
+            })
+
+        })
+    })
+
+});
+
+const show_pass_register = document.getElementById('show_pass_register');
+const show_pass_register_input = document.getElementById('show_pass_register_input');
+const show_pass_again_register = document.getElementById('show_pass_again_register_input');
+const show_pass_again_register_input = document.getElementById('show_pass_again_register_input');
+
+show_pass_register.addEventListener('click', () => {    
+
+    console.log(show_pass_register_input.type);
+
+    if(show_pass_register_input.getAttribute('type') == "password"){
+        show_pass_register_input.getAttribute('type', 'text')
+    }else{
+        show_pass_register_input.getAttribute('type', 'password')
+    }
+
+});
