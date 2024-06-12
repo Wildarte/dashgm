c69929127b4a12049597514da40861266a7f2b58
wildarte <wildarte10@gmail.com>
2024-06-11 23:02:07 -0300

    atualizacao

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,14 @@ const header = document.querySelector('header.header');
 const side_menu = document.querySelector('.side_menu');
 const body_dash = document.querySelector('.body_dash');
 if(header){
-const height_header =  header.getBoundingClientRect().height; //height do header   
+    const height_header =  header.getBoundingClientRect().height; //height do header  
+    side_menu.style.height = "calc(100vh - "+height_header+"px)";//margin top sidebar
+    side_menu.style.top = height_header+"px"; 
+    body_dash.style.height = "calc(100vh - "+height_header+"px)";
 }
 if(side_menu){
 
     const width_side_menu = side_menu.getBoundingClientRect().width; //width do sidemenu
-}
-
-
-if(side_menu){
-
-    side_menu.style.height = "calc(100vh - "+height_header+"px)";//margin top sidebar
-    side_menu.style.top = height_header+"px";
-}
-if(header){
-
-    body_dash.style.height = "calc(100vh - "+height_header+"px)";
     body_dash.style.width = "calc(100% - "+width_side_menu+"px)";
 }
 
